ab48429d01de7f1d89bd2305609ec5da4abdd101
Aetrix27 <davedogg323@gmail.com>
2021-03-17 15:55:53 -0700

    Finished main menu buttons

--- EasyHealth/Controllers/HomeViewController.swift
+++ EasyHealth/Controllers/HomeViewController.swift
@@ -9,23 +9,44 @@ import UIKit
 
 
 class HomeViewController: UIViewController {
-  
     
+    @IBAction func login(_ sender: Any) {
+        let vc = LoginViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        
+        nav.modalPresentationStyle = .fullScreen
+        present(nav, animated: false)
+    }
+    
+    @IBAction func register(_ sender: Any) {
+        let vc = RegisterViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        
+        nav.modalPresentationStyle = .fullScreen
+        present(nav, animated: false)
+        
+    }
+    
+    @IBAction func viewChats(_ sender: Any) {
+        let vc = ConversationsViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        
+        nav.modalPresentationStyle = .fullScreen
+        present(nav, animated: false)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        //let vc = segue.destination as? LoginViewController
+        
+     
+
+        
+        }
+    
 
 }
--- EasyHealth/Controllers/Login/LoginViewController.swift
+++ EasyHealth/Controllers/Login/LoginViewController.swift
@@ -59,6 +59,18 @@ class LoginViewController: UIViewController {
         
     }()
     
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.backgroundColor = .link
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        
+        return button
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -70,10 +82,17 @@ class LoginViewController: UIViewController {
                                                             target: self,
                                                             action: #selector(didTapRegister))
         
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        
+        emailField.delegate = self
+        passwordField.delegate = self
+        
+        
         view.addSubview(scrollView)
         scrollView.addSubview(imageView)
         scrollView.addSubview(emailField)
         scrollView.addSubview(passwordField)
+        scrollView.addSubview(loginButton)
 
     }
     
@@ -87,14 +106,34 @@ class LoginViewController: UIViewController {
                                  height: size)
         
         emailField.frame = CGRect(x: 30,
-                                  y: imageView.bottom+20,
+                                  y: imageView.bottom+25,
                                   width: scrollView.width-60,
                                   height: 52)
         
         passwordField.frame = CGRect(x: 30,
-                                  y: emailField.bottom+20,
+                                  y: emailField.bottom+25,
                                   width: scrollView.width-60,
                                   height: 52)
+        
+        loginButton.frame = CGRect(x: 30,
+                                  y: passwordField.bottom+25,
+                                  width: scrollView.width-60,
+                                  height: 52)
+    }
+    
+    func loginAlertError(){
+        let alert = UIAlertController(title: "Error", message: "Please enter all fields to log in.",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title:"Dismiss", style: .cancel, handler: nil))
+        present(alert, animated: true)
+    }
+    
+    @objc private func loginButtonTapped() {
+        guard let email = emailField.text, let password = passwordField.text,
+              !email.isEmpty, !password.isEmpty, password.count >= 6 else {
+                loginAlertError()
+                return
+        }
     }
     
     @objc private func didTapRegister(){
@@ -105,4 +144,18 @@ class LoginViewController: UIViewController {
     
 
   
+}
+
+extension LoginViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == emailField{
+            passwordField.becomeFirstResponder()
+        }
+        else if textField == passwordField{
+            loginButtonTapped()
+        }
+        
+        return true
+    }
 }
