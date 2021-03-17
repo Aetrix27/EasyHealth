92b625f463fbdc7188fc5e5b375e6870c8b2b94b
Aetrix27 <davedogg323@gmail.com>
2021-03-16 20:24:20 -0700

    Created email and password fields

--- EasyHealth/Controllers/Login/LoginViewController.swift
+++ EasyHealth/Controllers/Login/LoginViewController.swift
@@ -24,6 +24,41 @@ class LoginViewController: UIViewController {
         
     }()
     
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        //field.placeholder = "Email Address"
+        field.leftView = UIView(frame: CGRect(x: 0, y:0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        
+        return field
+        
+    }()
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        //field.placeholder = "Email Address"
+        field.leftView = UIView(frame: CGRect(x: 0, y:0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.isSecureTextEntry = true
+        
+        return field
+        
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,21 +66,35 @@ class LoginViewController: UIViewController {
 
         view.backgroundColor = .white
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self,
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done,
+                                                            target: self,
                                                             action: #selector(didTapRegister))
         
-        view.addSubview(imageView)
-        
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
 
     }
     
     override func viewDidLayoutSubviews(){
         super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
         let size = view.width/3
         imageView.frame = CGRect(x: (view.width-size)/2,
                                  y: 20,
                                  width: size,
                                  height: size)
+        
+        emailField.frame = CGRect(x: 30,
+                                  y: imageView.bottom+20,
+                                  width: scrollView.width-60,
+                                  height: 52)
+        
+        passwordField.frame = CGRect(x: 30,
+                                  y: emailField.bottom+20,
+                                  width: scrollView.width-60,
+                                  height: 52)
     }
     
     @objc private func didTapRegister(){
