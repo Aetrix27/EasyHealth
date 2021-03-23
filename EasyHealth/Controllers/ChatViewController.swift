00c24c0fd7e0ffef97a2b92b91dd9c5357e67628
Aetrix27 <davedogg323@gmail.com>
2021-03-22 22:37:11 -0700

    Created Chat View Controller

---
+++ EasyHealth/Controllers/ChatViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ChatViewController.swift
+//  EasyHealth
+//
+//  Created by David Guerrero on 3/22/21.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+
+
+}
--- EasyHealth/Controllers/ConversationsViewController.swift
+++ EasyHealth/Controllers/ConversationsViewController.swift
@@ -9,15 +9,35 @@ import UIKit
 import FirebaseAuth
 
 class ConversationsViewController: UIViewController {
-
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+    
+    private let noConversationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "No Chats"
+        label.textAlignment = .center
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 21, weight : .medium)
+        label.isHidden = true
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(tableView)
+        view.addSubview(noConversationLabel)
+        setupTableView()
+        fetchConversations()
         //navigationController?.navigationBar.topItem?.title = "Chats"
     }
 
     override func viewDidAppear(_ animated: Bool){
         super.viewDidAppear(animated)
-        
         validateAuth()
    
     }
@@ -31,6 +51,34 @@ class ConversationsViewController: UIViewController {
             
         }
     }
+    
+    private func setupTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func fetchConversations(){
+        
+    }
 
 }
 
+extension ConversationsViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section : Int) -> Int{
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:  indexPath)
+        cell.textLabel?.text = "Hello World"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = ChatViewController()
+        vc.title = "Tatiana "
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
